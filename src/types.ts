5d8df8ce40c7a586b6b4f414d99d8bbc456deb47
deepak kumar <deepakabes2006@gmail.com>
2025-04-16 11:43:08 +0530

    idfi

---
+++ src/types.ts
@@ -0,0 +1,17 @@
+export type CountryData = {
+    country: string;
+    phoneNumberLengthByCountry_phLengthMax: number |null;
+    phoneNumberLengthByCountry_phLengthMin: number | null;
+    phoneNumberLengthByCountry_phLength: string | number;
+    phoneNumberLengthByCountry_CountryCode: number | null;
+    phoneNumberLengthByCountry_phLength_InternationalPrefix: number | null;
+    phoneNumberLengthByCountry_phLength_NationalPrefix: number | null;
+    phoneNumberLengthByCountry_Notes: string | null;
+}
+
+export type NumberValidationData = {
+    country: string;
+    phLengthMax: number |null;
+    phLengthMin: number | null;
+    phExample?: string | number;
+}
